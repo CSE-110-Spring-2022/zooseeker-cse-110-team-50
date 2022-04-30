fe6049a0cea3fdc81f3f2be28710400da5596cee
Zichun Hao <zichunh2000@gmail.com>
2022-04-29 21:30:26 -0700

    Add the main activity for exhibit list

---
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/ExhibitListActivity.java
@@ -0,0 +1,14 @@
+package edu.ucsd.cse110.zooseeker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExhibitListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
