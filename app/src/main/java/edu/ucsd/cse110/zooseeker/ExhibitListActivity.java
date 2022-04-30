f60b5bcdef5d495883edad8fe3e05eee1b2c6138
oscarmtz1 <osmartin@ucsd.edu>
2022-04-29 23:13:57 -0700

    implemented exhibitList stuff

--- app/src/main/java/edu/ucsd/cse110/zooseeker/ExhibitListActivity.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/ExhibitListActivity.java
@@ -3,12 +3,24 @@ package edu.ucsd.cse110.zooseeker;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ExhibitListActivity extends AppCompatActivity {
+    public RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exhibit_list);
 
+        ExhibitListAdapter adapter = new ExhibitListAdapter();
+        adapter.setHasStableIds(true);
+
+        recyclerView = findViewById(R.id.exhibit_items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setTodoListItems(TodoListItem.loadJSON(this, 'sample_node_info.json'));
 }
