40cea728b1a8e76a791684c182358beed1b394f2
Zichun Hao <zichunh2000@gmail.com>
2022-04-30 10:51:26 -0700

    problems

--- app/src/main/java/edu/ucsd/cse110/zooseeker/Persistence/Exhibit.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/Persistence/Exhibit.java
@@ -4,28 +4,29 @@ import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+
 @Entity(tableName = "exhibit")
 public class Exhibit {
 
-    @PrimaryKey(autoGenerate = false)
-    public String exhibitId;
-
-    @NonNull
-    public String name;
-    public String kind;
-
-    Exhibit(@NonNull String id, String name, String kind) {
-        this.exhibitId = id;
-        this.name = name;
-        this.kind = kind;
-    }
-
-    @Override
-    public String toString() {
-        return "Exhibit{" +
-                "id='" + exhibitId + '\'' +
-                ", name='" + name + '\'' +
-                ", kind='" + kind + '\'' +
-                '}';
-    }
+//    @PrimaryKey(autoGenerate = false)
+//    public String exhibitId;
+//
+//    @NonNull
+//    public String name;
+//    public String kind;
+//
+//    Exhibit(@NonNull String id, String name, String kind, List<String> tags) {
+//        this.exhibitId = id;
+//        this.name = name;
+//        this.kind = kind;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Exhibit{" +
+//                "id='" + exhibitId + '\'' +
+//                ", name='" + name + '\'' +
+//                ", kind='" + kind + '\'' +
+//                '}';
+//    }
 }
