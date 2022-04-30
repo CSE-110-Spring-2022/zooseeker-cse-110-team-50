9bbe0bebd6cb14776d5a967649537f2dab4edef3
oscarmtz1 <osmartin@ucsd.edu>
2022-04-30 00:07:55 -0700

    added some stuff

--- app/src/main/java/edu/ucsd/cse110/zooseeker/SearchListActivity.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/SearchListActivity.java
@@ -1,7 +1,14 @@
 package edu.ucsd.cse110.zooseeker;
 
-public class SearchListActivity {
+import android.os.Bundle;
 
-    //@Override
+import androidx.appcompat.app.AppCompatActivity;
 
+public class SearchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
