546a9033f30a6e9fd1808ff34c97b06e207ba369
hyun04p <hyun04.p@gmail.com>
2022-04-29 21:25:24 -0700

    implements bottom navigation layout

---
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/Search/SearchFragment.java
@@ -0,0 +1,38 @@
+package edu.ucsd.cse110.zooseeker.Search;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.ucsd.cse110.zooseeker.R;
+
+public class SearchFragment extends Fragment {
+
+    private SearchViewModel mViewModel;
+
+    public static SearchFragment newInstance() {
+        return new SearchFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.search_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SearchViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/Search/SearchViewModel.java
@@ -0,0 +1,7 @@
+package edu.ucsd.cse110.zooseeker.Search;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
