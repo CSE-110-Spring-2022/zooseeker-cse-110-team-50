348982f4a9a02c5688ece0d83ca00a73feb8eee7
oscarmtz1 <osmartin@ucsd.edu>
2022-04-29 22:31:47 -0700

    added files

---
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/ViewHolder.java
@@ -0,0 +1,20 @@
+package edu.ucsd.cse110.zooseeker;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder{
+
+    private final TextView textView;
+    private ExhibitListItem exhibitItem;
+
+    public ViewHolder(@NonNull View itemView){
+        super(itemView);
+        this.textView = itemView.findViewById(R.id.exhibit_item_txt);
+    }
+
+    public ExhibitListItem getExhibitItem(){return exhibitItem;}
+}
