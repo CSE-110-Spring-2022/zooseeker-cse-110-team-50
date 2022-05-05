87d63a5d3e49a20819d099b03925fce1d9dbf189
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-05-04 17:47:41 -0700

    [#72] Implement Button Plan List to Route

--- app/src/main/java/edu/ucsd/cse110/zooseeker/Plan/PlanFragment.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/Plan/PlanFragment.java
@@ -1,5 +1,6 @@
 package edu.ucsd.cse110.zooseeker.Plan;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,20 +8,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import edu.ucsd.cse110.zooseeker.MainActivity;
 import edu.ucsd.cse110.zooseeker.R;
+import edu.ucsd.cse110.zooseeker.RouteActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link PlanFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PlanFragment extends Fragment {
+public class PlanFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private Button startRoute;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,6 +66,18 @@ public class PlanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plan, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_plan, container, false);
+
+        startRoute = view.findViewById(R.id.start_route_button);
+        startRoute.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getActivity(), RouteActivity.class);
+        //myIntent.putExtra("key", value); //Optional parameters
+        startActivity(intent);
     }
 }
