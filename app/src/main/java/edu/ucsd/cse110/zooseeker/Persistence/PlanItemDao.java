031ba2d1e3114eb5f8d1184b16c592726e0e1d80
Zichun Hao <zichunh2000@gmail.com>
2022-05-05 15:59:52 -0700

    [User Story 4] Fix: Ignore the duplicated insertion

--- app/src/main/java/edu/ucsd/cse110/zooseeker/Persistence/PlanItemDao.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/Persistence/PlanItemDao.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Dao
 public interface PlanItemDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     long insert(PlanItem planItem);
 
     @Query("SELECT * FROM `plan_list` WHERE `placeId`=:placeId")
