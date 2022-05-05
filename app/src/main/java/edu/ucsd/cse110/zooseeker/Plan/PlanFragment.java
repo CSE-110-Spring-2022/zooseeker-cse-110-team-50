10ee719eec6fa0c1fa37e30892a49ec4e8630029
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-05-04 22:26:07 -0700

    Added Tests

--- app/src/main/java/edu/ucsd/cse110/zooseeker/Plan/PlanFragment.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/Plan/PlanFragment.java
@@ -39,17 +39,11 @@ public class PlanFragment extends Fragment implements View.OnClickListener {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment PlanFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static PlanFragment newInstance(String param1, String param2) {
+    public static PlanFragment newInstance() {
         PlanFragment fragment = new PlanFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
