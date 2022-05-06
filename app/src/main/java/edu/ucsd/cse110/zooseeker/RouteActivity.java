078ec514efffde8e3277be2d8bf3834d393c7eb1
Zichun Hao <zichunh2000@gmail.com>
2022-05-05 23:16:28 -0700

    [User Story 4] Update: Stay updated with develop branch

---
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/RouteActivity.java
@@ -0,0 +1,14 @@
+package edu.ucsd.cse110.zooseeker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+    }
+}
