151d03b9b5ae57eb3608bc36ebf685c4131c4e2f
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-05-21 04:16:40 -0700

    Story 1i, 1ii, and 2 finished

--- app/src/main/java/edu/ucsd/cse110/zooseeker/Route/RouteActivity.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/Route/RouteActivity.java
@@ -26,6 +26,9 @@ public class RouteActivity extends AppCompatActivity {
     private List<Router.RoutePackage> pkgList;
     private int routeIndex = 0;
     private PlaceDao placeDao = MainDatabase.getSingleton(this).placeDao();
+    boolean isDetailedDirections = true;
+    TextView routeTextView;
+    Button toggleDirectionsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +37,7 @@ public class RouteActivity extends AppCompatActivity {
 
         Button nextButton = findViewById(R.id.route_next_button);
 
-        Button toggleDirectionsButton = findViewById(R.id.toggle_directions_button);
+        toggleDirectionsButton = findViewById(R.id.toggle_directions_button);
 
         nextButton.setOnClickListener(new View.OnClickListener()
         {
@@ -71,22 +74,42 @@ public class RouteActivity extends AppCompatActivity {
         List<PlanItem> allPlanItems = planItemDao.getAll();
         List<String> allNodes = allPlanItems.stream().map((item) -> item.placeId).collect(Collectors.toList());
         pkgList = router.route(allNodes);
+        toggleDirectionsButton.setText("Brief\nDirections");
 //        for (Router.RoutePackage pkg : pkgList) {
 //            routeStr += pkg.toString() + "\n";
 //        }
 
-        TextView routeTextView = findViewById(R.id.route_text_view);
-        routeTextView.setText(pkgList.get(routeIndex).toStringDetailed());
+        routeTextView = findViewById(R.id.route_text_view);
+        if(isDetailedDirections){
+            routeTextView.setText(pkgList.get(routeIndex).toStringDetailed());
+        }
+        else{
+            routeTextView.setText(pkgList.get(routeIndex).toStringBrief());
+        }
     }
 
     public void toggleDirections(View v){
-
+        if(isDetailedDirections){
+            routeTextView.setText(pkgList.get(routeIndex).toStringBrief());
+            isDetailedDirections = false;
+            toggleDirectionsButton.setText("Detailed\nDirections");
+        }
+        else{
+            routeTextView.setText(pkgList.get(routeIndex).toStringDetailed());
+            isDetailedDirections = true;
+            toggleDirectionsButton.setText("Brief\nDirections");
+        }
     }
 
     public void nextExhibit(View v){
         if(routeIndex + 1 < pkgList.size()) routeIndex++;
 
-        TextView routeTextView = findViewById(R.id.route_text_view);
-        routeTextView.setText(pkgList.get(routeIndex).toStringDetailed());
+        routeTextView = findViewById(R.id.route_text_view);
+        if(isDetailedDirections){
+            routeTextView.setText(pkgList.get(routeIndex).toStringDetailed());
+        }
+        else{
+            routeTextView.setText(pkgList.get(routeIndex).toStringBrief());
+        }
     }
 }
--- app/src/main/java/edu/ucsd/cse110/zooseeker/Util/Router/Router.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/Util/Router/Router.java
@@ -92,17 +92,43 @@ public class Router {
         public String toStringBrief() {
             int cnt = 1;
             String ret = "From " + start + "\n\n\n";
-            for (RouteStep step : steps) {
+            List<String> compressedEdges = new ArrayList();
+            List<Double> compressedDistances = new ArrayList();
+            List<String> endOfPaths = new ArrayList();
+            int index = -1;
 
+            for(RouteStep step : steps){
                 if(start == step.to){
                     continue;
                 }
-                ret += (cnt++ + ". Proceed on " +
-                        edgeInfo.get(step.edgeId) +
-                        " " + step.distance +
-                        " ft toward " + step.to + "\n\n");
+                if(index == -1){
+                    // first time. We will add the place and the distances
+                    compressedEdges.add(edgeInfo.get(step.edgeId));
+                    index = 0;
+                    compressedDistances.add(step.distance);
+                    endOfPaths.add(step.to);
+                    continue;
+                }
+                if(!compressedEdges.get(index).equals(edgeInfo.get(step.edgeId))){
+                    // We are in a new place. Add to the compressedEdges
+                    index++;
+                    compressedEdges.add(edgeInfo.get(step.edgeId));
+                    compressedDistances.add(step.distance);
+                    endOfPaths.add(step.to);
+                }
+                else{
+                    compressedDistances.set(index, compressedDistances.get(index) + step.distance);
+                    endOfPaths.set(index, step.to);
+                }
             }
-
+            index = 0;
+            for (String edge : compressedEdges){
+                ret += (cnt++ + ". Proceed on " +
+                        edge +
+                        " " + compressedDistances.get(index) +
+                        " ft toward " + endOfPaths.get(index) + "\n\n");
+                index++;
+            }
             ret += "\nDestination: " + end + "\n";
             return ret;
         }
