ff2215ac99e05a81ea299ea4fa11cfeab9da8c61
Zichun Hao <zichunh2000@gmail.com>
2022-05-29 15:56:31 -0700

    [#145] Fix Parent Id not parsing properly

--- app/src/main/java/edu/ucsd/cse110/zooseeker/Util/JSONLoader/NodeInfoMapper.java
+++ app/src/main/java/edu/ucsd/cse110/zooseeker/Util/JSONLoader/NodeInfoMapper.java
@@ -1,5 +1,7 @@
 package edu.ucsd.cse110.zooseeker.Util.JSONLoader;
 
+import android.util.Log;
+
 import java.util.List;
 
 import edu.ucsd.cse110.zooseeker.Persistence.Place;
@@ -9,12 +11,26 @@ public class NodeInfoMapper {
     public String id;
     public String kind;
     public String name;
-    public String parentId;
+    public String parent_id;
     public List<String> tags;
     public Double lat;
     public Double lng;
 
     public Place toPlace() {
-        return new Place(id, name, kind, parentId, lng, lat);
+        Log.d("NodeInfoMapper", toString());
+        return new Place(id, name, kind, parent_id, lng, lat);
+    }
+
+    @Override
+    public String toString() {
+        return "NodeInfoMapper{" +
+                "id='" + id + '\'' +
+                ", kind='" + kind + '\'' +
+                ", name='" + name + '\'' +
+                ", parent_id='" + parent_id + '\'' +
+                ", tags=" + tags +
+                ", lat=" + lat +
+                ", lng=" + lng +
+                '}';
     }
 }
